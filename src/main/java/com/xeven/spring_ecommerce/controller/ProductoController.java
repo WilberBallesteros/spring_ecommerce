108cba491e3510b0ad11c12da27cd95574e8c053
Wilber Ballesteros <ballestas01@gmail.com>
2023-04-26 09:57:59 -0500

    Se crea la funcionalidad para editar un registro de tipo producto, realizando el metodo edit y update en el ProductoController, modificando las vistas para realizar el correcto funcionamiento delas plantillas

--- src/main/java/com/xeven/spring_ecommerce/controller/ProductoController.java
+++ src/main/java/com/xeven/spring_ecommerce/controller/ProductoController.java
@@ -4,11 +4,13 @@ package com.xeven.spring_ecommerce.controller;
 import com.xeven.spring_ecommerce.model.Producto;
 import com.xeven.spring_ecommerce.model.Usuario;
 import com.xeven.spring_ecommerce.service.ProductoService;
+import java.util.Optional;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,6 +43,24 @@ public class ProductoController {
         producto.setUsuario(u);
         productoService.save(producto);
         return "redirect:/productos"; //carga la vista show
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+        
+        LOGGER.info("P´roducto buscado: {}", producto);
+        model.addAttribute("producto", producto);
+        return "productos/edit";
+    }
+    
+    @PostMapping("/update")
+    public String update(Producto producto) {
+        productoService.update(producto);
+        return "redirect:/productos";
     }
     
 }
