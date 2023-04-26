63149b5575ae081f57f933861c415d7f694319f2
Wilber Ballesteros <ballestas01@gmail.com>
2023-04-26 10:03:18 -0500

    Se crea la funcionalidad para eliminar un registro de tipo producto, se crea un metodo delete en el controlador de productos

--- src/main/java/com/xeven/spring_ecommerce/controller/ProductoController.java
+++ src/main/java/com/xeven/spring_ecommerce/controller/ProductoController.java
@@ -63,4 +63,10 @@ public class ProductoController {
         return "redirect:/productos";
     }
     
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        
+        productoService.delete(id);
+        return "redirect:/productos";
+    }
 }
