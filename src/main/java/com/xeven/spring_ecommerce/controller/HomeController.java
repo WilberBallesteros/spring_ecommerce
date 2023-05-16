1ccb373d20054ed627f1f4a2d6bb879f6fcc9a74
Wilber Ballesteros <ballestas01@gmail.com>
2023-05-16 16:12:38 -0500

    Mostramos la vista del producto al usuario al presionar click en el boton ver producto

--- src/main/java/com/xeven/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/xeven/spring_ecommerce/controller/HomeController.java
@@ -2,15 +2,20 @@
 package com.xeven.spring_ecommerce.controller;
 
 import com.xeven.spring_ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    
+    private final Logger log =LoggerFactory.getLogger(HomeController.class);
     
     @Autowired
     private ProductoService productoService; //obtener los productos
@@ -20,5 +25,14 @@ public class HomeController {
         
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+    
+    //cuando le demos click al boton ver producto nos lleve a la vista ver producto
+    @GetMapping("productohome/{id}")
+    public String ProductoHome(@PathVariable Integer id) { //@PathVariable el id lo toma de la url
+        
+        log.info("Id producto enviado como parametro {}", id);
+        
+        return "usuario/productohome";
     }
 }
